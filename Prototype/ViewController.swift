ea8a1eb42711b6858bb400a7441db5a274fa1687
Rakeeb <Rakeeb@dhcp-18-111-108-16.dyn.mit.edu>
2018-07-30 08:57:23 -0400

    Initial Commit

---
+++ Prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Rakeeb on 2018-07-30.
+//  Copyright © 2018 Rakeeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
